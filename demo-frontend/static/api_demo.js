f3a22f38d32271072c0619ca1f097da646b44a7e
Hanna Kollo <hannakollo@google.com>
2024-07-22 16:00:07 +0000

    Extracted the javascript to a file

---
+++ demo-frontend/static/api_demo.js
@@ -0,0 +1,77 @@
+// Logic for the buttons on the form
+
+/* -------- Handling the "Save" button -------- */
+
+
+/* -------- Handling the "Find matching jobs" button -------- */
+
+const jobsButton = document.getElementById('button-find-jobs');
+const inputElement = document.getElementById('free-form-descr-e1');
+
+function send_user_input() {
+  console.log("Sending '" + inputElement.value + "' to the server...");
+  jobsButton.style.color = "grey";
+
+  fetch('api/candidate_info_instant', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json' },
+    body: JSON.stringify({userid: "user42", occupations: [{name: "occupation1", description: inputElement.value}] }),
+  })
+  .then(response => {
+    console.log('Candidate input data is sent to the server. Response code: ' + response.status);
+    jobsButton.style.color = "pink";
+    if (!response.ok) {
+      throw new Error('API request failed with status: ${response.status}');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Data: ' + data);
+    console.log('Content: ' + data.content);
+    show_results(data.content);
+  })
+  .catch(error => {
+    console.error('Error: ', error);
+  });
+};
+
+function show_results(suggestion_list) {
+  const containerNode = document.getElementById('suggestionContainer');
+  const jobTemplateNode = document.getElementById('jobTemplate');
+
+  suggestion_list.forEach(job => {
+    console.log('Job:' + job.job_title);
+    const jobDiv = jobTemplateNode.cloneNode(true);
+    jobDiv.querySelector('.jobTitle').textContent = job.job_title;
+    jobDiv.querySelector('.jobDescription').textContent = job.job_description;
+    jobDiv.querySelector('.jobLink').textContent = job.link;
+    containerNode.appendChild(jobDiv);
+  });
+  jobTemplateNode.style.display = 'none';
+  containerNode.style.display = 'block';
+  }
+
+// Add a click event listener to the jobs button.
+jobsButton.addEventListener('click', () => {
+  console.log("Button clicked");
+  event.preventDefault()
+  send_user_input();
+});
+
+// Add "press Enter" listener to the input box
+inputElement.addEventListener('keydown', function(event) {
+  if (event.key === "Enter") {
+    send_user_input()
+  }
+});
+
+/* -------- Handling the "Identify my skills" button -------- */
+
+const skillButton = document.getElementById('button-identify-skills');
+
+// Add a click event listener to the skill button.
+skillButton.addEventListener('click', () => {
+  console.log("Skill button clicked");
+  event.preventDefault()
+});
+
